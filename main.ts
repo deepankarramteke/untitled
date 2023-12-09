ba6887104401f4ea796f246c76795f564725e7ca
deepankarramteke <77459296+deepankarramteke@users.noreply.github.com>
2023-12-09 16:24:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+enum RadioMessage {
+    message1 = 49434,
+    Deepankar = 4874
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    basic.showString("~~OO~~OO~~OO~~OO~~")
+})
